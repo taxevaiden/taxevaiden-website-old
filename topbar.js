da59d311a4f7cab7694ab33c9d832e6720aa9581
aiden <aideniscool473@gmail.com>
2024-06-09 13:55:09 -0400

    initial commit!
    add alllllllllll the files...

---
+++ topbar.js
@@ -0,0 +1,36 @@
+const topbarDiv = document.createElement("div");
+topbarDiv.className = "topbar";
+
+//home
+const home = document.createElement("a");
+home.href = "index.html";
+const homeImage = document.createElement("img");
+homeImage.src = "images/icons/home.png";
+homeImage.className = "icon";
+home.appendChild(homeImage);
+
+
+//about
+const about = document.createElement("a");
+about.href = "about.html";
+const aboutImage = document.createElement("img");
+aboutImage.src = "images/icons/about.png";
+aboutImage.className = "icon";
+about.appendChild(aboutImage);
+
+
+//projects
+const projects = document.createElement("a");
+projects.href = "#";
+const projectsImage = document.createElement("img");
+projectsImage.src = "images/icons/projects.png";
+projectsImage.className = "icon";
+projects.appendChild(projectsImage);
+
+
+topbarDiv.appendChild(projects);
+topbarDiv.appendChild(about);
+topbarDiv.appendChild(home);
+
+const body = document.getElementById("main");
+body.appendChild(topbarDiv);
