3dd646c6168a099b228a8bd520f29e6f013dfcd3
aiden <aideniscool473@gmail.com>
2024-06-15 13:11:55 -0400

    add new "blogs" page
    Yes.

---
+++ js/posts.js
@@ -0,0 +1,29 @@
+var postSummary = function (link, title, date)
+{
+    var container = document.createElement("div");
+    container.className = "blogSummary";
+
+    var blogTitle = document.createElement("h2");
+    var titleA = document.createElement("a");
+    titleA.href = link;
+    var titleContents = document.createTextNode(`${title}`);
+    titleA.appendChild(titleContents);
+    blogTitle.appendChild(titleA);
+    
+
+    var blogDate = document.createElement("p");
+    var dateString = document.createTextNode(`${date}`)
+    blogDate.appendChild(dateString);
+
+    container.appendChild(blogTitle);
+    container.appendChild(blogDate);
+
+    var blogsContainer = document.getElementById("blogsContainer");
+
+    blogsContainer.appendChild(container);
+}
+
+var addPosts = function ()
+{
+    postSummary("posts/first-post.html", "first post!!", "6/15/2024");
+}
--- js/topbar.js
+++ js/topbar.js
@@ -1,36 +1,70 @@
-const topbarDiv = document.createElement("div");
-topbarDiv.className = "topbar";
+var createTopbar = function (isblog) {
+  const topbarDiv = document.createElement("div");
+  topbarDiv.className = "topbar";
 
-//home
-const home = document.createElement("a");
-home.href = "index.html";
-const homeImage = document.createElement("img");
-homeImage.src = "images/icons/home.png";
-homeImage.className = "icon";
-home.appendChild(homeImage);
+  //home
+  const home = document.createElement("a");
+  
+  const homeImage = document.createElement("img");
+  if (isblog)
+    {
+        home.href = "../index.html";
+        homeImage.src = "../images/icons/home.png";
+    } else {
+        home.href = "index.html";
+        homeImage.src = "images/icons/home.png";
+    }
+  
+  homeImage.className = "icon";
+  home.appendChild(homeImage);
 
+  //about
+  const about = document.createElement("a");
+  const aboutImage = document.createElement("img");
+  if (isblog)
+    {
+        about.href = "../about.html";
+        aboutImage.src = "../images/icons/about.png";
+    } else {
+        about.href = "about.html";
+        aboutImage.src = "images/icons/about.png";
+    }
+  aboutImage.className = "icon";
+  about.appendChild(aboutImage);
 
-//about
-const about = document.createElement("a");
-about.href = "about.html";
-const aboutImage = document.createElement("img");
-aboutImage.src = "images/icons/about.png";
-aboutImage.className = "icon";
-about.appendChild(aboutImage);
+  //projects
+  const projects = document.createElement("a");
+  const projectsImage = document.createElement("img");
+  if (isblog)
+    {
+        projects.href = "../projects.html";
+        projectsImage.src = "../images/icons/projects.png";
+    } else {
+        projects.href = "projects.html";
+        projectsImage.src = "images/icons/projects.png";
+    }
+  projectsImage.className = "icon";
+  projects.appendChild(projectsImage);
 
+  //blogs
+  const blogs = document.createElement("a");
+  const blogsImage = document.createElement("img");
+  if (isblog)
+    {
+          blogs.href = "../blogs.html";
+          blogsImage.src = "../images/icons/blogs.png";
+    } else {
+      blogs.href = "blogs.html";
+        blogsImage.src = "images/icons/blogs.png";
+    }
+    blogsImage.className = "icon";
+    blogs.appendChild(blogsImage);
 
-//projects
-const projects = document.createElement("a");
-projects.href = "projects.html";
-const projectsImage = document.createElement("img");
-projectsImage.src = "images/icons/projects.png";
-projectsImage.className = "icon";
-projects.appendChild(projectsImage);
+  topbarDiv.appendChild(blogs);
+  topbarDiv.appendChild(projects);
+  topbarDiv.appendChild(about);
+  topbarDiv.appendChild(home);
 
-
-topbarDiv.appendChild(projects);
-topbarDiv.appendChild(about);
-topbarDiv.appendChild(home);
-
-const body = document.getElementById("main");
-body.appendChild(topbarDiv);
+  const body = document.getElementById("main");
+  body.appendChild(topbarDiv);
+};
