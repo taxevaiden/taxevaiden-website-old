9fdd8a1498c07bd0b21a6f42d7fedcc14a271357
aiden <aideniscool473@gmail.com>
2024-06-17 16:48:54 -0400

    clean up + bugb gvbu
    i hate bugs they always fly in my ear

--- js/posts.js
+++ js/posts.js
@@ -27,7 +27,7 @@ var postSummary = function (link, title, date)
 
 var addPosts = function ()
 {
-    postSummary("posts/i-need-ideas", "i need ideas.", "6/17/2024");
+    postSummary("posts/i-need-ideas.html", "i need ideas.", "6/17/2024");
     postSummary("posts/changes-to-the-website.html", "changes to the website", "6/16/2024");
     postSummary("posts/first-post.html", "first post!!", "6/15/2024");
 }
@@ -54,66 +54,7 @@ var addExtrasEnd = function ()
 {
     var main_text = document.getElementById("blog_div");
 
-    main_text.innerHTML += `<!-- customize labels of htmlcommentbox.com -->
-<script>
-    /* This code goes ABOVE the main HTML Comment Box code!
-     replace the text in the single quotes below to customize labels.*/
-    hcb_user = {
-        /* L10N */
-        comments_header : 'comments!',
-        name_label : 'name',
-        content_label: 'enter your comment here',
-        submit : 'comment!!',
-        logout_link : '<img title="log out" src="https://www.htmlcommentbox.com/static/images/door_out.svg" alt="[logout]" class="hcb-icon hcb-door-out"/>',
-        admin_link : '<img src="https://www.htmlcommentbox.com/static/images/door_in.svg" alt="[login]" class="hcb-icon hcb-door-in"/>',
-        no_comments_msg: "no has commented. comment first!! or don't. i don't care",
-        add:'add your comment',
-        again: 'post another comment',
-        rss:'<img src="https://www.htmlcommentbox.com/static/images/feed.svg" class="hcb-icon" alt="rss"/> ',
-        said:'said:',
-        prev_page:'<img src="https://www.htmlcommentbox.com/static/images/arrow_left.png" class="hcb-icon" title="previous page" alt="[prev]"/>',
-        next_page:'<img src="https://www.htmlcommentbox.com/static/images/arrow_right.png" class="hcb-icon" title="next page" alt="[next]"/>',
-        showing:'showing',
-        to:'to',
-        website_label:'website (optional)',
-        email_label:'email',
-        anonymous:'Anonymous',
-        mod_label:'(mod)',
-        subscribe:'Email Me Replies',
-        add_image:'Add Image',
-        are_you_sure:'you sure this is bad? if so, ahead and flag it.',
-    
-        reply:'reply',
-        flag:'flag',
-        like:'like',
-    
-        /* dates */
-        days_ago:'days ago',
-        hours_ago:'hours ago',
-        minutes_ago:'minutes ago',
-        within_the_last_minute:'within the last minute',
-    
-        msg_thankyou:'thank you for commenting!',
-        msg_approval:'oh ya quick note: you have to wait for your comment to be approved',
-        msg_approval_required:'thank you for commenting! wait until this is approved and then it will appear',
-    
-        err_bad_html:'you comment contained bad html.',
-        err_bad_email:'please enter a valid email address.',
-        err_too_frequent:'slow down!',
-        err_comment_empty:"you can't just comment empty ones, that's so plain..",
-        err_denied:'your comment was not accepted.',
-        err_unknown:'your comment was blocked for unknown reasons, please report this.',
-        err_spam:'you comment got detected as spam.',
-        err_blocked:'your comment was blocked by site policy.',
-    
-        /* SETTINGS */
-        MAX_CHARS: 8192,
-        PAGE:'', /* ID of the webpage to show comments for. defaults to the webpage the user is currently visiting. */
-        ON_COMMENT: function(){}, /* Function to call after commenting. */
-        RELATIVE_DATES:true /* show dates in the form "X hours ago." etc. */
-    };
-    </script>
-    <!-- done customizing labels of htmlcommentbox.com -->
+    main_text.innerHTML += `
 
             <!-- begin wwww.htmlcommentbox.com -->
  <div id="HCB_comment_box"><a href="http://www.htmlcommentbox.com">Widget</a> is loading comments...</div>
