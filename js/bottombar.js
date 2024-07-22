618338c6695630d4961def25aa2ee5480127b4b8
aiden <aideniscool473@gmail.com>
2024-07-22 18:32:27 -0400

    i'm even more generous

--- js/bottombar.js
+++ js/bottombar.js
@@ -3,7 +3,7 @@ var createBottomBar = function () {
     bottomBarDiv.className = "bottombar";
   
     var note = document.createElement("p");
-    note.innerHTML = "&copy; 2024 aiden/taxevaiden, unless noted, all rights reserved.<br>background in durpydoo theme created by winterkin_ (it's just durpydoo's pfp lmao)";
+    note.innerHTML = "website coded by me, aiden (or taxevaiden idk<br>background in durpydoo theme created by winterkin_ (it's just durpydoo's pfp lmao)";
 
     bottomBarDiv.appendChild(note);
   
