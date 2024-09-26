6768a00609185045034d444654d6a4d762500fda
aiden <aideniscool473@gmail.com>
2024-09-25 21:14:46 -0400

    wow

--- js/topbar.js
+++ js/topbar.js
@@ -1,28 +1,27 @@
 var createTopbar = function (isblog) {
   const body = document.getElementById("main");
-  if (isblog)
-    {
-      body.innerHTML += `<div id="menu_bar" class="menu_div closed">
+  if (isblog) {
+    body.innerHTML += `<div id="menu_bar" class="menu_div closed">
       <a id="close"><img class="icon" src="../images/icons/menu.png"></a>
       <a href="../index.html"><img class="icon" src="../images/icons/home.png">home</a>
       <a href="../about.html"><img class="icon" src="../images/icons/about.png">about</a>
       <a href="../projects.html"><img class="icon" src="../images/icons/projects.png">projects</a>
       <a href="../blogs.html"><img class="icon" src="../images/icons/blogs.png">blog</a>
     </div>`;
-    } else {
-      body.innerHTML += `<div id="menu_bar" class="menu_div closed">
+  } else {
+    body.innerHTML += `<div id="menu_bar" class="menu_div closed">
       <a id="close"><img class="icon" src="images/icons/menu.png">close</a>
       <a href="index.html"><img class="icon" src="images/icons/home.png">home</a>
       <a href="about.html"><img class="icon" src="images/icons/about.png">about</a>
       <a href="projects.html"><img class="icon" src="images/icons/projects.png">projects</a>
       <a href="blogs.html"><img class="icon" src="images/icons/blogs.png">blog</a>
     </div>`;
-    }
-    
-    const menuBarButton = document.getElementById("close");
-    menuBarButton.onclick = function() {
-      this.parentElement.className = "menu_div closed";
-    }
+  }
+
+  const menuBarButton = document.getElementById("close");
+  menuBarButton.onclick = function () {
+    this.parentElement.className = "menu_div closed";
+  };
 
   const topbarDiv = document.createElement("div");
   topbarDiv.className = "topbar";
@@ -30,38 +29,25 @@ var createTopbar = function (isblog) {
 
   //home
   const home = document.createElement("a");
-  
-  const homeImage = document.createElement("img");
-  if (isblog)
-    {
-        home.href = "../index.html";
-        homeImage.src = "../images/icons/home.png";
-    } else {
-        home.href = "index.html";
-        homeImage.src = "images/icons/home.png";
-    }
-  homeImage.className = "icon";
+  if (isblog) {
+    home.href = "../index.html";
+  } else {
+    home.href = "index.html";
+  }
   const homeText = document.createElement("p");
-  homeText.appendChild(homeImage);
   const homeTextContents = document.createTextNode("home");
   homeText.appendChild(homeTextContents);
-  homeText.className = "topbar-a-text"; 
+  homeText.className = "topbar-a-text";
   home.appendChild(homeText);
 
   //about
   const about = document.createElement("a");
-  const aboutImage = document.createElement("img");
-  if (isblog)
-    {
-        about.href = "../about.html";
-        aboutImage.src = "../images/icons/about.png";
-    } else {
-        about.href = "about.html";
-        aboutImage.src = "images/icons/about.png";
-    }
-  aboutImage.className = "icon";
+  if (isblog) {
+    about.href = "../about.html";
+  } else {
+    about.href = "about.html";
+  }
   const aboutText = document.createElement("p");
-  aboutText.appendChild(aboutImage);
   const aboutTextContents = document.createTextNode("about");
   aboutText.appendChild(aboutTextContents);
   aboutText.className = "topbar-a-text";
@@ -69,64 +55,49 @@ var createTopbar = function (isblog) {
 
   //projects
   const projects = document.createElement("a");
-  const projectsImage = document.createElement("img");
-  if (isblog)
-    {
-        projects.href = "../projects.html";
-        projectsImage.src = "../images/icons/projects.png";
-    } else {
-        projects.href = "projects.html";
-        projectsImage.src = "images/icons/projects.png";
-    }
-    const projectsText = document.createElement("p");
-    projectsText.appendChild(projectsImage);
-    const projectsTextContents = document.createTextNode("projects");
-    projectsText.appendChild(projectsTextContents);
-    projectsText.className = "topbar-a-text";
-  projectsImage.className = "icon";
+  if (isblog) {
+    projects.href = "../projects.html";
+  } else {
+    projects.href = "projects.html";
+  }
+  const projectsText = document.createElement("p");
+  const projectsTextContents = document.createTextNode("projects");
+  projectsText.appendChild(projectsTextContents);
+  projectsText.className = "topbar-a-text";
   projects.appendChild(projectsText);
 
   //blogs
   const blogs = document.createElement("a");
-  const blogsImage = document.createElement("img");
-  if (isblog)
-    {
-          blogs.href = "../blogs.html";
-          blogsImage.src = "../images/icons/blogs.png";
-    } else {
-      blogs.href = "blogs.html";
-        blogsImage.src = "images/icons/blogs.png";
-    }
-    const blogsText = document.createElement("p");
-    blogsText.appendChild(blogsImage);
-    const blogsTextContents = document.createTextNode("blog");
-    blogsText.appendChild(blogsTextContents);
-    blogsText.className = "topbar-a-text";
-  blogsImage.className = "icon";
+  if (isblog) {
+    blogs.href = "../blogs.html";
+  } else {
+    blogs.href = "blogs.html";
+  }
+  const blogsText = document.createElement("p");
+  const blogsTextContents = document.createTextNode("blog");
+  blogsText.appendChild(blogsTextContents);
+  blogsText.className = "topbar-a-text";
   blogs.appendChild(blogsText);
 
   const menu = document.createElement("a");
   const menuImage = document.createElement("img");
-  if (isblog)
-    {
-      menuImage.src = "../images/icons/menu.png";
-    } else {
-      menuImage.src = "images/icons/menu.png";
-    }
-    const menuText = document.createElement("p");
-    menuText.appendChild(menuImage);
-    const menuTextContents = document.createTextNode("menu");
-    menuText.appendChild(menuTextContents);
-    menuText.className = "topbar-a-text";
-    menuImage.className = "icon";
-    menu.appendChild(menuText)
-    menu.id = "menu_icon";
-    menu.onclick = function() {
-      const menuBar = document.getElementById("menu_bar"); 
-      menuBar.className = "menu_div";
-    };
-
-  
+  if (isblog) {
+    menuImage.src = "../images/icons/menu.png";
+  } else {
+    menuImage.src = "images/icons/menu.png";
+  }
+  const menuText = document.createElement("p");
+  menuText.appendChild(menuImage);
+  const menuTextContents = document.createTextNode("menu");
+  menuText.appendChild(menuTextContents);
+  menuText.className = "topbar-a-text";
+  menuImage.className = "icon";
+  menu.appendChild(menuText);
+  menu.id = "menu_icon";
+  menu.onclick = function () {
+    const menuBar = document.getElementById("menu_bar");
+    menuBar.className = "menu_div";
+  };
 
   topbarDiv.appendChild(home);
   topbarDiv.appendChild(about);
@@ -134,8 +105,5 @@ var createTopbar = function (isblog) {
   topbarDiv.appendChild(blogs);
   topbarDiv.appendChild(menu);
 
-  
   body.appendChild(topbarDiv);
-
-  
 };
